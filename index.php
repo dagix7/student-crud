2f49df797edc044f8bc6cc63d115d81d0b5c8353
Dagim Asnake <139282456+dagix7@users.noreply.github.com>
2025-06-07 08:28:52 +0300

    first commit

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ if (isset($_GET['error'])) {
         }
         .table-responsive {
             box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.15);
-            border-radius: 0.25rem;
+            border-radius: 0.2rem;
         }
         .page-title {
             color: #2c3e50;
